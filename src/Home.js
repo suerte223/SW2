f69945149b5a69b773fb839a0321956971e146c7
ouskxk <01.hyeona@gmail.com>
2025-10-30 16:10:24 +0900

    코드 수정

--- src/Home.js
+++ src/Home.js
@@ -38,70 +38,70 @@ export default function Home() {
         <p className="subtitle">
           AI와 함께 매일의 식단, 활동, 건강을 체계적으로 기록하고 관리해보세요.
         </p>
-        <a className="cta" href="/signup">지금 시작하기</a>
+        <a className="cta" href="/Signin">지금 시작하기</a>
       </main>
 
-      {/* 푸터 */}
-      <footer className="footer">
-        <div className="footer-inner">
-            <div className="logo-row">
+<footer className="footer">
+  <div className="footer-inner">
+    <div className="logo-row">
 
-            <div className="logo-stack">
-                <img src={logoGray} alt="" className="paw-bg" aria-hidden />
-                <span className="wordmark">KoJJOK</span>
-            </div>
-            <div className="grid">
-                <div className="col">
-                <h3>Hyeona Kim</h3>
-                <p>UI/UX Design</p>
-                <a href="https://github.com/ouskxk" className="github-link">
-                    <img src={githubpic} alt="GitHub Logo" className="github-icon" />
-                    ouskxk
-                </a>
-                </div>
-                <div className="col">
-                    <h3>Jiun Ko</h3>
-                    <p>Front-End Dev</p>
-                    <a href="https://github.com/suerte223" className="github-link">
-                        <img src={githubpic} alt="GitHub Logo" className="github-icon" />
-                        suerte223
-                    </a>
-                </div>
-                <div className="col">
-                    <h3>Seungbeom Han</h3>
-                    <p>Front-End Dev</p>
-                    <a href="https://github.com/hsb9838" className="github-link">
-                        <img src={githubpic} alt="GitHub Logo" className="github-icon" />
-                        hsb9838
-                    </a>
-                </div>
-                <div className="col">
-                    <h3>Munjin Yang</h3>
-                    <p>Back-End Dev</p>
-                    <a href="https://github.com/munjun0608" className="github-link">
-                        <img src={githubpic} alt="GitHub Logo" className="github-icon" />
-                        munjun0608
-                    </a>
-                </div>
-                <div className="col">
-                    <h3>Youngbin Kang</h3>
-                    <p>Back-End Dev</p>
-                    <a href="https://github.com/0bini" className="github-link">
-                        <img src={githubpic} alt="GitHub Logo" className="github-icon" />
-                        0bini
-                    </a>
-                </div>
-            </div>
-
-            {/* 그룹 3: 기술 스택 */}
-            <div className="tech-stack">
-                <h3>TECH STACK</h3>
-                <img src={reactpic} alt="React Logo" className="react-icon" />
-                <img src={djangopic} alt="Django Logo" className="django-icon" />
-            </div>
-            </div>
+      <div className="logo-stack">
+        <img src={logoGray} alt="" className="paw-bg" aria-hidden />
+        <span className="wordmark">KoJJOK</span>
+      </div>
+      <div className="grid">
+        <div className="col">
+          <h3>Hyeona Kim</h3>
+          <p>UI/UX Design</p>
+          <a href="https://github.com/ouskxk" className="github-link">
+            <img src={githubpic} alt="GitHub Logo" className="github-icon" />
+            ouskxk
+          </a>
         </div>
-        </footer>
+        <div className="col">
+            <h3>Jiun Ko</h3>
+            <p>Front-End Dev</p>
+            <a href="https://github.com/suerte223" className="github-link">
+                <img src={githubpic} alt="GitHub Logo" className="github-icon" />
+                suerte223
+            </a>
+        </div>
+        <div className="col">
+            <h3>Seungbeom Han</h3>
+            <p>Front-End Dev</p>
+            <a href="https://github.com/hsb9838" className="github-link">
+                <img src={githubpic} alt="GitHub Logo" className="github-icon" />
+                hsb9838
+            </a>
+        </div>
+        <div className="col">
+            <h3>Munjin Yang</h3>
+            <p>Back-End Dev</p>
+            <a href="https://github.com/munjun0608" className="github-link">
+                <img src={githubpic} alt="GitHub Logo" className="github-icon" />
+                munjun0608
+            </a>
+        </div>
+        <div className="col">
+            <h3>Youngbin Kang</h3>
+            <p>Back-End Dev</p>
+            <a href="https://github.com/0bini" className="github-link">
+                <img src={githubpic} alt="GitHub Logo" className="github-icon" />
+                0bini
+            </a>
+        </div>
+      </div>
+
+      {/* 그룹 3: 기술 스택 */}
+      <div className="tech-stack">
+        <h3>TECH STACK</h3>
+        <img src={reactpic} alt="React Logo" className="react-icon" />
+        <img src={djangopic} alt="Django Logo" className="django-icon" />
+      </div>
+    </div>
+  </div>
+</footer>
+
     </div>
   );
 }
